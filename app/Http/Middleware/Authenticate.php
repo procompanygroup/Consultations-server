dd7be13cf0e9d4f80238de5b356de07472e034d5
ahmadms2020 <70610331+ahmadms2020@users.noreply.github.com>
2023-12-28 17:22:59 +0200

    error msg

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -10,8 +10,13 @@ class Authenticate extends Middleware
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      */
+    protected function unauthenticated($request, array $guards)
+    {
+        abort(response()->json(['error' => 'Unauthenticated'], 401));
+    }
     protected function redirectTo(Request $request): ?string
     {
         return $request->expectsJson() ? null : route('login');
     }
+
 }
