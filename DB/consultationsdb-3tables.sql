715e40c7aa2ed81b6d49038d82db1d57fbf6dcec
ahmadms2020 <70610331+ahmadms2020@users.noreply.github.com>
2024-01-09 18:03:01 +0200

    migrate

---
+++ DB/consultationsdb-3tables.sql
@@ -0,0 +1,238 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Jan 09, 2024 at 03:59 PM
+-- Server version: 8.0.31
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `consultationsdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `clients`
+--
+
+DROP TABLE IF EXISTS `clients`;
+CREATE TABLE IF NOT EXISTS `clients` (
+  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `mobile` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `nationality` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `birthdate` datetime DEFAULT NULL,
+  `gender` int DEFAULT NULL,
+  `marital_status` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `image` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `token` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `points_balance` int DEFAULT '0',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `clients_user_name_unique` (`user_name`),
+  UNIQUE KEY `clients_mobile_unique` (`mobile`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `clients`
+--
+
+INSERT INTO `clients` (`id`, `user_name`, `password`, `mobile`, `email`, `nationality`, `birthdate`, `gender`, `marital_status`, `image`, `token`, `points_balance`, `created_at`, `updated_at`) VALUES
+(1, 'ahmad', '$2y$12$oXfs1CEoT.FEy7iNvZk3TuDgQkSqgcarXF8o/fS.7Qt.zIMrzl6bS', '0945994666', 'ahmad@gmail.com', 'ksa', NULL, NULL, 'single', NULL, NULL, 0, NULL, NULL),
+(2, 'ahmad2', '$2y$12$oXfs1CEoT.FEy7iNvZk3TuDgQkSqgcarXF8o/fS.7Qt.zIMrzl6bS', '0945994876', 'ahmad2@gmail.com', 'ksa', NULL, NULL, 'single', NULL, NULL, 0, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `experts`
+--
+
+DROP TABLE IF EXISTS `experts`;
+CREATE TABLE IF NOT EXISTS `experts` (
+  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `mobile` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `nationality` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `birthdate` datetime DEFAULT NULL,
+  `gender` int DEFAULT NULL,
+  `marital_status` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `image` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `points_balance` int DEFAULT '0',
+  `cash_balance` decimal(8,2) DEFAULT '0.00',
+  `cash_balance_todate` decimal(8,2) DEFAULT '0.00',
+  `rates` decimal(8,2) DEFAULT '0.00',
+  `record` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `desc` text COLLATE utf8mb4_unicode_ci,
+  `call_cost` int DEFAULT '0',
+  `password` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `experts_user_name_unique` (`user_name`),
+  UNIQUE KEY `experts_mobile_unique` (`mobile`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `experts`
+--
+
+INSERT INTO `experts` (`id`, `user_name`, `mobile`, `email`, `nationality`, `birthdate`, `gender`, `marital_status`, `image`, `points_balance`, `cash_balance`, `cash_balance_todate`, `rates`, `record`, `desc`, `call_cost`, `password`, `token`, `created_at`, `updated_at`) VALUES
+(1, 'expert', '06919696', 'ahmad@gmail.com', 'syr', '2000-01-22 22:01:24', 2, 'single', NULL, 0, '0.00', '0.00', '0.00', NULL, 'expert', 0, '$2y$12$oXfs1CEoT.FEy7iNvZk3TuDgQkSqgcarXF8o/fS.7Qt.zIMrzl6bS', '', '2024-01-01 20:01:24', '2024-01-01 20:01:24'),
+(2, 'expert2', '069196326', 'ahmad2@gmail.com', 'uae', '2000-01-22 22:01:24', 2, 'single', NULL, 0, '0.00', '0.00', '0.00', NULL, 'expert', 0, '$2y$12$oXfs1CEoT.FEy7iNvZk3TuDgQkSqgcarXF8o/fS.7Qt.zIMrzl6bS', '', '2024-01-01 20:01:24', '2024-01-01 20:01:24');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `failed_jobs`
+--
+
+DROP TABLE IF EXISTS `failed_jobs`;
+CREATE TABLE IF NOT EXISTS `failed_jobs` (
+  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
+  `uuid` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+DROP TABLE IF EXISTS `migrations`;
+CREATE TABLE IF NOT EXISTS `migrations` (
+  `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
+  `migration` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2014_10_12_000000_create_users_table', 1),
+(2, '2014_10_12_100000_create_password_reset_tokens_table', 1),
+(3, '2014_10_12_100000_create_password_resets_table', 1),
+(4, '2019_08_19_000000_create_failed_jobs_table', 1),
+(5, '2019_12_14_000001_create_personal_access_tokens_table', 1),
+(6, '2024_01_06_193109_create_experts_table', 1),
+(7, '2024_01_06_193700_create_clients_table', 1),
+(8, '2024_01_07_145557_add_token_to_experts_table', 2),
+(9, '2024_01_07_145557_add_password_to_experts_table', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_resets`
+--
+
+DROP TABLE IF EXISTS `password_resets`;
+CREATE TABLE IF NOT EXISTS `password_resets` (
+  `email` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  KEY `password_resets_email_index` (`email`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_reset_tokens`
+--
+
+DROP TABLE IF EXISTS `password_reset_tokens`;
+CREATE TABLE IF NOT EXISTS `password_reset_tokens` (
+  `email` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`email`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `personal_access_tokens`
+--
+
+DROP TABLE IF EXISTS `personal_access_tokens`;
+CREATE TABLE IF NOT EXISTS `personal_access_tokens` (
+  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
+  `tokenable_type` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tokenable_id` bigint UNSIGNED NOT NULL,
+  `name` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `abilities` text COLLATE utf8mb4_unicode_ci,
+  `last_used_at` timestamp NULL DEFAULT NULL,
+  `expires_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
+  KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `email` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `first_name` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `last_name` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `user_name` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `role` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `token` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `createuser_id` bigint UNSIGNED DEFAULT NULL,
+  `updateuser_id` bigint UNSIGNED DEFAULT NULL,
+  `mobile` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_email_unique` (`email`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `first_name`, `last_name`, `user_name`, `role`, `token`, `createuser_id`, `updateuser_id`, `mobile`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'ahmad333', 'najyms@gmail.com', NULL, '$2y$12$oXfs1CEoT.FEy7iNvZk3TuDgQkSqgcarXF8o/fS.7Qt.zIMrzl6bS', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2024-01-07 16:47:22', '2024-01-07 16:47:22'),
+(2, 'ahmad2333', 'ahmad2@gmail.com', NULL, '$2y$12$x1tFQq8C2USr3TVtNijNAOUfPCvzS1vEU2iLM6gaQG03GA5Q9/i4e', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2024-01-07 16:49:17', '2024-01-07 16:49:17');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
