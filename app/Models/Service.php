1d570458b9ecba241ea50bbce1c67b9b34872134
ahmadms2020 <70610331+ahmadms2020@users.noreply.github.com>
2024-01-11 17:11:01 +0200

    Update Service.php

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+ 
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Service extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        
+        'name',
+        'desc',
+        'image',
+        'createuser_id',
+        'updateuser_id',
+             
+    ];
+
+ 
 }
