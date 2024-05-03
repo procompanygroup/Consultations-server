13a31e14c17c1187e4f0e90e8744c24284088b65
najims2016 <70610331+ahmadms2020@users.noreply.github.com>
2024-05-03 12:22:00 +0200

    Update NotificationController.php

--- app/Http/Controllers/Api/NotificationController.php
+++ app/Http/Controllers/Api/NotificationController.php
@@ -77,7 +77,8 @@ class NotificationController extends Controller
           'username' => "Ahmad",
           'image' => $defaultimg,
         ])
-        ->sendMessage($tokenList);
+       // ->sendMessage($tokenList)
+        ->sendNotification($tokenList);
     } else {
       return 'empty token';
     }
