8be9bf14af526cae9c7ac0cf20e2d261e7cd16c5
najims2016 <70610331+ahmadms2020@users.noreply.github.com>
2024-07-02 23:46:05 +0200

    Update verify-template.blade.php

--- resources/views/admin/mail/verify-template.blade.php
+++ resources/views/admin/mail/verify-template.blade.php
@@ -1,31 +1,47 @@
 <!DOCTYPE html>
 <html lang="ar" dir="rtl">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contact Email</title>
+    <title>رسالة تحقق</title>
     <style>
-    .thtitle{
-        text-align: right;
-    padding: 5px;
-    }
+        .head-line {
+            height: 15px;
+            background-color: #E72167;
+        }
+        .footer-line {
+            height: 15px;
+            background-color: lightgray;
+            margin-top: 20px;
+        }
+        .site-name {
+            text-align: center;
+            font-size: 30px;
+            color: #E72167;
+            padding: 10px;
+        }
+        .code-style {
+            padding-left: 5px;
+            padding-right: 5px;
+        }
+        .code-div {
+            text-align: right;
+            font-size: 16px;
+            padding: 10px;
+        }
     </style>
 </head>
 <body>
-<div class="row"></div>
-<div class="col-12">
-<div style="height: 15px; background-color: #E72167;"></div>
-    <p class="lead" style="text-align: center; font-size:20px;padding:10px">
-        <img  src="{{$data['logo']}}" width="50px" height="50px" alt="logo"></p>
-    <div class="lead" style="text-align: right; font-size:16px;padding:10px">
-        <span>رمز التحقق:</span><strong style="padding-left:5px;padding-right:5px;">{{$data['code']}}</strong>
+    <div class="row"></div>
+    <div class="col-12">
+        <div class="head-line" style=""></div>
+        <p class="lead site-name">{{ $data['com_title'] }}</p>
+        <div class="lead code-div">
+            <span>رمز التحقق:</span><strong class="code-style">{{ $data['code'] }}</strong>
+        </div>
+        <div class="footer-line"></div>
     </div>
-
-    
-  
-    <div style="height: 15px; background-color: lightgray;margin-top:20px"></div>
-  </div>
- 
 </body>
 </html>
