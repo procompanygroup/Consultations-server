952d07f74236b8495da0aa03278daba85816f4bc
najims2016 <naji58809@gmail.com>
2024-09-02 16:15:10 +0300

    logo

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <title>تطبيق روح </title>
-
+    <link rel="icon" href="{{URL::asset('assets/site/img/logo-title.svg')}}" type="image/x-icon">
    
 <!--- Style css -->
 <link href="{{URL::asset('assets/site/css/style.css')}}" rel="stylesheet">
