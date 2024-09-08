36eb8e3d5d1d33a3f1719f22ba52221fe9b7412f
najims2016 <naji58809@gmail.com>
2024-09-08 15:27:49 +0300

    Update NotifyController.php

--- app/Http/Controllers/Web/NotifyController.php
+++ app/Http/Controllers/Web/NotifyController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Carbon\Traits\ToStringFormat;
 use Illuminate\Http\Request;
 use Google\Client as GoogleClient;
+use GuzzleHttp\Client as HttpClient;
 class NotifyController extends Controller
 {
     public function sendbytoken(Request $request)
@@ -36,66 +37,68 @@ class NotifyController extends Controller
 
     }
 
-    public function send_to_fcm($to_token, $title, $body, $dataArr = null)
-    {
-        if (is_null($to_token) || $to_token == '') {
-            return 'no-token';
-        } else {
-            $credentialsFilePath = storage_path('app/rouh-app-firebase-adminsdk-cecz8-895e8731d2.json');
+    // public function send_to_fcm($to_token, $title, $body, $dataArr = null)
+    // {
+    //     if (is_null($to_token) || $to_token == '') {
+    //         return 'no-token';
+    //     } else {
+    //         $credentialsFilePath = storage_path('app/rouh-app-firebase-adminsdk-cecz8-895e8731d2.json');
 
-            $client = new GoogleClient();
+    //         $client = new GoogleClient();
 
-            $client->setAuthConfig($credentialsFilePath);
+    //         $client->setAuthConfig($credentialsFilePath);
 
-            $client->addScope('https://www.googleapis.com/auth/firebase.messaging');
-            //  $client->refreshTokenWithAssertion();
-            $token = $client->fetchAccessTokenWithAssertion();
-            // $token = $client->getAccessToken();
+    //         $client->addScope('https://www.googleapis.com/auth/firebase.messaging');
+    //         //  $client->refreshTokenWithAssertion();
+    //       //  $token = $client->fetchAccessTokenWithAssertion();
+    //         $client->refreshTokenWithAssertion();
+    //           $token = $client->getAccessToken();
 
-            $access_token = $token['access_token'];
+    //         $access_token = $token['access_token'];
 
-            // Set up the HTTP headers
-            $headers = [
-                "Authorization: Bearer $access_token",
-                'Content-Type: application/json'
-            ];
-            $data = [
-                "message" => [
-                    'token' => $to_token,
-                    // 'registration_ids'=>[$token_to],
-                    "notification" => [
-                                           "title" => $title,
-                                         "body" => $body,                                        
-                                       ],
-                                       "data" =>$dataArr,
-                    "apns" => [
-                        "payload" => [
-                            "aps" => [
-                                "sound" => "default"
-                            ],
+    //         // Set up the HTTP headers
+    //         $headers = [
+    //             "Authorization: Bearer $access_token",
+    //             'Content-Type: application/json'
+    //         ];
+    //         $data = [
+    //             "message" => [
+    //                 'token' => $to_token,
+    //                 'notification' => [
+    //                     'title' => $title,
+    //                     'body' => $body,                        
+    //                 ],
+    //                 'data' => $dataArr,                                       
+    //                                    ]
+    //         ];
+    //       //  $payload = json_encode($data);
+    //      $postcmnd= new HttpClient();
+    //      $response=  $postcmnd->post('https://fcm.googleapis.com/v1/projects/rouh-app/messages:send', [
+    //             'headers' => [
+    //                 'Authorization' => 'Bearer ' . $access_token,
+    //                 'Content-Type' => 'application/json',
+    //             ],
+    //             'json' =>$data,
+    //         ]);
 
-                        ]
-                    ]
-                ]
-            ];
-            $payload = json_encode($data);
 
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, 'https://fcm.googleapis.com/v1/projects/rouh-app/messages:send');
-            curl_setopt($ch, CURLOPT_POST, true);
-            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
-            curl_setopt($ch, CURLOPT_VERBOSE, false); // Enable verbose output for debugging
-            $response = curl_exec($ch);
-            $err = curl_error($ch);
-            curl_close($ch);
-            return $response;
+    //         // $ch = curl_init();
+    //         // curl_setopt($ch, CURLOPT_URL, 'https://fcm.googleapis.com/v1/projects/rouh-app/messages:send');
+    //         // curl_setopt($ch, CURLOPT_POST, true);
+    //         // curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    //         // curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    //         // curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    //         // curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+    //         // curl_setopt($ch, CURLOPT_VERBOSE, false); // Enable verbose output for debugging
+    //         // $response = curl_exec($ch);
+    //         // $err = curl_error($ch);
+    //         // curl_close($ch);
+    //       //  return $response ;
+    //    //   return $response->getStatusCode();
+    //       return   $response->getBody()->getContents();
+    //     }
 
-        }
-
-    }
+    // }
 
     // public function send_to_fcm($to_token, $title, $body, $dataArr = null)
     // {
@@ -158,4 +161,59 @@ class NotifyController extends Controller
     //     }
 
     // }
+
+
+      public function send_to_fcm($to_token, $title, $body, $dataArr = null)
+    {
+        if (is_null($to_token) || $to_token == '') {
+            return 'no-token';
+        } else {
+            $credentialsFilePath = storage_path('app/rouh-app-firebase-adminsdk-cecz8-895e8731d2.json');
+
+            $client = new GoogleClient();
+
+            $client->setAuthConfig($credentialsFilePath);
+
+            $client->addScope('https://www.googleapis.com/auth/firebase.messaging');
+            //  $client->refreshTokenWithAssertion();
+            $token = $client->fetchAccessTokenWithAssertion();
+            $client->refreshTokenWithAssertion();
+          $token = $client->getAccessToken();
+
+            $access_token = $token['access_token'];
+
+            // Set up the HTTP headers
+            $headers = [
+                "Authorization: Bearer $access_token",
+                'Content-Type: application/json'
+            ];
+            $data = [
+                "message" => [
+                    'token' => $to_token,
+                    // 'registration_ids'=>[$token_to],
+                    "notification" => [
+                        "title" => $title,
+                        "body" => $body,
+                    ],
+                    "data" => $dataArr,                
+                ]
+            ];
+            $payload = json_encode($data);
+
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, 'https://fcm.googleapis.com/v1/projects/rouh-app/messages:send');
+            curl_setopt($ch, CURLOPT_POST, true);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+            curl_setopt($ch, CURLOPT_VERBOSE, false); // Enable verbose output for debugging
+            $response = curl_exec($ch);
+            $err = curl_error($ch);
+            curl_close($ch);
+            return $response;
+
+        }
+
+    }
 }
